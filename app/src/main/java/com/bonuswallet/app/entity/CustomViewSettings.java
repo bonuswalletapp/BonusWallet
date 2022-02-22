c32ea26a9e1598fd061443234e2ce06ee753d877
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-02-22 02:06:55 -0500

    hide dappweb brwser

--- app/src/main/java/com/bonuswallet/app/entity/CustomViewSettings.java
+++ app/src/main/java/com/bonuswallet/app/entity/CustomViewSettings.java
@@ -106,7 +106,7 @@ public class CustomViewSettings
     //Switch off dapp browser
     public static boolean hideDappBrowser()
     {
-        return false;
+        return true;
     }
 
     //Hides the filter tab bar at the top of the wallet screen (ALL/CURRENCY/COLLECTIBLES)
